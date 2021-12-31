0ec7da0c6d3c97b4197815269ef9361c560b8a1e
MJ redfield <fieldr62@gmail.com>
2021-12-31 18:04:06 +0200

    mj

---
+++ JS/crew.js
@@ -0,0 +1,67 @@
+var picks_url =  ['../assets/crew/image-douglas-hurley.png','../assets/crew/image-mark-shuttleworth.png','../assets/crew/image-victor-glover.png','../assets/crew/image-anousheh-ansari.png']
+var jobs = ['COMMANDER','MISSION SPECIALIST','PILOT','FLIGHT ENGINEER']
+var names = ['DOUGLAS HURLEY','MARK SHUTTLEWORTH','VICTOR GLOVER','ANOUSHEH ANSARI']
+var descs = ['Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.','Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.','Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer. ','Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space. ']
+var pos = 0
+
+var img = document.getElementById('image')
+var job = document.getElementById('job')
+var name = document.getElementById('name')
+var desc = document.getElementById('desc')
+
+var ov1 = document.getElementById('ov1')
+var ov2 = document.getElementById('ov2')
+var ov3 = document.getElementById('ov3')
+var ov4 = document.getElementById('ov4')
+
+var selects = document.getElementsByClassName('ov')
+selects[pos].classList.add('ovv')
+
+
+ov1.addEventListener('click',function(){
+    
+    selects[pos].classList.remove('ovv')
+    pos = 0
+    img.src = picks_url[pos]
+    job.innerText = jobs[pos]
+    desc.innerText = descs[pos]
+    document.getElementsByClassName('doug')[0].innerHTML=names[pos]
+    selects[pos].classList.add('ovv')
+    
+})
+
+ov2.addEventListener('click',function(){
+    
+    selects[pos].classList.remove('ovv')
+    pos = 1
+    img.src = picks_url[pos]
+    job.innerText = jobs[pos]
+    desc.innerText = descs[pos]
+    document.getElementsByClassName('doug')[0].innerHTML=names[pos]
+    selects[pos].classList.add('ovv')
+    
+})
+
+ov3.addEventListener('click',function(){
+    
+    selects[pos].classList.remove('ovv')
+    pos = 2
+    img.src = picks_url[pos]
+    job.innerText = jobs[pos]
+    desc.innerText = descs[pos]
+    document.getElementsByClassName('doug')[0].innerHTML=names[pos]
+    selects[pos].classList.add('ovv')
+    
+})
+
+ov4.addEventListener('click',function(){
+    
+    selects[pos].classList.remove('ovv')
+    pos = 3
+    img.src = picks_url[pos]
+    job.innerText = jobs[pos]
+    desc.innerText = descs[pos]
+    document.getElementsByClassName('doug')[0].innerHTML=names[pos]
+    selects[pos].classList.add('ovv')
+    
+})
---
+++ JS/des.js
@@ -0,0 +1,73 @@
+
+var pos = 0
+var dess = ["MOON","MARS","EUROPA","TITAN"]
+var infos = ["See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites."
+,"Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
+"The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
+"The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn."]
+var distances = ["384,400 km","225 MIL. km","628 MIL. km","1.6 BIL. km"]
+var days =["3 DAYS","9 MONTHS","3 YEARS","7 YEARS"]
+
+var pdays = document.getElementById('day')
+var pdis = document.getElementById('dis')
+var pinfos = document.getElementById('see')
+var pdess = document.getElementById('pm')
+
+pdays.innerText = days[pos]
+pdis.innerText = distances[pos]
+pinfos.innerText = infos[pos]
+pdess.innerText = dess[pos]
+
+
+
+
+var moon = document.getElementById('moon')
+var mars = document.getElementById('mars')
+var europa = document.getElementById('europa')
+var titan = document.getElementById('titan')
+
+var selects = document.getElementsByClassName('sp')
+
+selects[pos].classList.add('select')
+
+moon.addEventListener('click',function(){
+    selects[pos].classList.remove('select')
+    pos = 0
+    pdays.innerText = days[pos]
+pdis.innerText = distances[pos]
+pinfos.innerText = infos[pos]
+pdess.innerText = dess[pos]
+selects[pos].classList.add('select')
+})
+mars.addEventListener('click',function(){
+    selects[pos].classList.remove('select')
+    pos = 1
+    pdays.innerText = days[pos]
+pdis.innerText = distances[pos]
+pinfos.innerText = infos[pos]
+pdess.innerText = dess[pos]
+selects[pos].classList.add('select')
+})
+europa.addEventListener('click',function(){
+    selects[pos].classList.remove('select')
+    pos = 2
+    pdays.innerText = days[pos]
+pdis.innerText = distances[pos]
+pinfos.innerText = infos[pos]
+pdess.innerText = dess[pos]
+selects[pos].classList.add('select')
+})
+titan.addEventListener('click',function(){
+    selects[pos].classList.remove('select')
+    pos = 3
+    pdays.innerText = days[pos]
+pdis.innerText = distances[pos]
+pinfos.innerText = infos[pos]
+pdess.innerText = dess[pos]
+selects[pos].classList.add('select')
+})
+
+
+
+
+
